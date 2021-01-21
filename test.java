fd06fafe4a133ab65875269a0e35b912e2e5b7b4
JenniferYang2020 <jenniferyang1990@hotmail.com>
2021-01-21 15:05:25 +0800

    AddFourFiles

---
+++ test.java
@@ -0,0 +1,18 @@
+class test{
+
+    public static void main(String[] args){
+     
+        // byte b=2;
+        // b=(byte)(b+2);
+        // System.out.println(b);
+
+        int x, x1=1;
+        x=x1+Integer.MAX_VALUE;
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MAX_VALUE+10);
+        
+        } 
+
+
+}
