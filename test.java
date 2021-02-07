bfad710182584ec5bd9c337fe9904f87597d8182
JenniferYang2020 <jenniferyang1990@hotmail.com>
2021-02-07 09:45:23 +0800

    change1

--- test.java
+++ test.java
@@ -6,13 +6,22 @@ class test{
         // b=(byte)(b+2);
         // System.out.println(b);
 
-        int x, x1=1;
-        x=x1+Integer.MAX_VALUE;
+        // int x, x1=1;
+        // x=x1+Integer.MAX_VALUE;
 
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MAX_VALUE+10);
-        
+        // System.out.println(Integer.MAX_VALUE);
+        // System.out.println(Integer.MAX_VALUE+10);
+        String s="abc";
+
+        System.out.println(s.substring(0,3));
+
         } 
-
+ public static void main(String[] args) {
+     
+ }
 
 }
+
+
+
+
